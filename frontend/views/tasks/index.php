995395b8ae9ee2ac3c6da60ab6f529c9c0d8b749
naryzhnaj <naryzhnaj@gmail.com>
2019-11-13 03:09:11 +0300

    исправлены иконки

--- frontend/views/tasks/index.php
+++ frontend/views/tasks/index.php
@@ -1,7 +1,7 @@
 <?php
 /* @var $this yii\web\View */
 use yii\helpers\Html;
-$now = new DateTime("now");
+$now = new DateTime();
 $this->title = 'recent tasks';
 ?>
 <div class="page-container">
@@ -14,9 +14,9 @@ $this->title = 'recent tasks';
                   <a href="#" class="link-regular"><h2><?=Html::encode($task->title); ?></h2></a>
                   <a class="new-task__type link-regular" href="#"><p><?=Html::encode($task->category->title); ?></p></a>
               </div>
-              <div class="new-task__icon new-task__icon--clean"></div>
+              <div class="new-task__icon new-task__icon--<?=Html::encode($task->category->icon); ?>"></div>
               <p class="new-task_description"><?=Html::encode($task->description); ?></p>
-              <b class="new-task__price new-task__price--clean"><?=Html::encode($task->budget); ?><b> ₽</b></b>
+              <b class="new-task__price new-task__price--<?=Html::encode($task->category->icon); ?>"><?=Html::encode($task->budget); ?><b> ₽</b></b>
               <p class="new-task__place"><?=Html::encode($task->address); ?></p>
               <span class="new-task__time"><?=$now->diff(new DateTime($task->dt_add))->format('%d дней %h часов назад'); ?></span>
           </div>
